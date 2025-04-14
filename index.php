6d6b162447f6e6ba1ef539ece64f37492f6df717
Paralizers <darkparalizer@gmail.com>
2025-04-14 19:53:09 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+function scaricaImmagine($url, $proxy = null) {
+$url = "https://gosoccerboy5.github.io/view-images/#".$url;
+echo file_get_contents($url);exit;print_r($url);
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_REFERER, $url);
+    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36");
+  
+
+
+
+curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION,true);
+    $imageData = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+    curl_close($ch);
+
+    if ($httpCode >= 400 || !$imageData) {
+        return false;
+    }
+
+    header("Content-Type: $contentType");
+    echo $imageData;
+    return true;
+}
+
+$url = $_GET['img'] ?? null;
+if (!$url) {
+    http_response_code(400);
+    exit("Parametro 'img' mancante.");
+}
+
+// Prova senza proxy
+if (!scaricaImmagine($url)) {
+ 
+
+    http_response_code(500);
+    exit("Impossibile scaricare l'immagine anche tramite proxy.");
+}
+?>
